5ef60c936e88536142e46e47cc0c4a8dc52ce67a
Axelum <Axelum@AXELUM->
2018-06-12 21:05:16 +0700

    Тест побуждающий создать класс модели

---
+++ tdd/TestFolder/test.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    
+    namespace tdd
+{
+    [TestFixture]
+    class test
+    {
+        /// <summary>
+        /// Тест побуждающий создать класс модели
+        /// </summary>
+        [Test]
+        public void BarkleyGameCreationTest()
+        {
+            BarkleyModel bm = new BarkleyModel();
+            Assert.NotNull(bm);
+        }
+    }
+}
